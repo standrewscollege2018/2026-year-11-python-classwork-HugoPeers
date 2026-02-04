759e5c6808d4fe47008496f3513916d9bc5e6c6f
HugoPeers <hpe7595@stacmail.net>
2026-02-05 12:04:21 +1300

    print statement on intro file

---
+++ Python main.py
@@ -0,0 +1,8 @@
+''' This program demonstrates print(), data types, variables, inputs and f-strings '''
+
+#print( ) is a function that outputs whatever is inside the brackets
+# nmbers can be included directly in the berackets
+print (1234)
+
+# When printing text, it must be in speechmarks which turns it into a string
+print("Hello")
