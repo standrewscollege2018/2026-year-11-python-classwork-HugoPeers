a1b7327a60082b549412de1464ae6337a3f2b46a
HugoPeers <hpe7595@stacmail.net>
2026-02-12 12:31:19 +1300

    if statements

---
+++ if.py
@@ -0,0 +1,12 @@
+''' Demonstrating how a condition statement (if/else) works 
+it asks for a password and then checks if it is correct'''
+
+# Ask for password and store in a variable 
+password = input ("enter your password")
+
+#check if it is correct
+if password == "carrots" :
+    print ("correct password")
+else:
+    print ("incorect. Get out!")
+    
